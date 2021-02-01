90a5136b122c731ab9d6c1b512c1e44c071f28a5
tanguydlz <59443450+tanguydlz@users.noreply.github.com>
2021-02-01 19:40:25 +0100

    V2

--- Projet_TM/server.R
+++ Projet_TM/server.R
@@ -2,9 +2,9 @@
     setwd("D:/Téléchargements/M2/Text mining")
 
 
-server <- function(input, output) {
+server <- function(input, output, session) {
     df<-read.xlsx2("BDD_CEDA_dec_2020_complete - ANONYME.xlsx", sheetIndex = 1,header = TRUE)
-    new_df<-data.frame(df$Préoccupations,df$Motif.de.consult,df$Diagnostic.,df$jeux.et.activités,df$Sommeil,df$Entrée.en.contact,df$Habitudes.de.l.enfant,df$Qui,df$Antécédents,df$X1eres.inquiétudes..mois.)
+    new_df<-df[,15:16]
     
     
     FEEL_lex <- read_csv2("http://advanse.lirmm.fr/FEEL.csv")
@@ -16,7 +16,7 @@ server <- function(input, output) {
     
 
 
-    
+    #Word plot data
     var <- reactive({
         tidytext = text_df %>% unnest_tokens(word, as.numeric(input$selectVar))
         tidytext = tidytext[,-2]
@@ -27,6 +27,7 @@ server <- function(input, output) {
     
     })
     
+    #Sentiment analysis data
     feel <- reactive({
         tidytext = text_df %>% unnest_tokens(word, as.numeric(input$selectVar2))
         tidytext = tidytext[,-2]
@@ -34,9 +35,36 @@ server <- function(input, output) {
         test_feel_df <- inner_join(test_clean_df, FEEL_lex, by = 'word')
     })
     
+    #n-gram visualization data
+    ngram <- reactive ({
+      tidytext <- text_df %>% unnest_tokens(bigram, as.numeric(input$selectVar3),token = "ngrams", n = 2)
+      tidytext=tidytext[,-2]
+      
+        bigrams_separated <- tidytext %>%
+        separate(bigram, c("word1", "word2"), sep = " ")
+      
+        bigrams_filtered <- bigrams_separated %>%
+          filter(!word1 %in% c(stopwords('french'),'a','très','d\'un','qu\'il','nc','avoir','afin')) %>%
+          filter(!word2 %in% c(stopwords('french'),'a','très','d\'un','qu\'il','nc','avoir','afin')) %>%
+          filter(!is.na(word1)) %>%
+          filter(!is.na(word2))
+        
+          bigram_counts <- bigrams_filtered %>% 
+            count(word1, word2, sort = TRUE)
+    })
     
+
     
-    
+    #Corrplot data
+    corr <- reactive ({
+      tidytext = text_df %>% unnest_tokens(word, as.numeric(input$selectVar4))
+      tidytext = tidytext[,-2]
+      test_clean_df <- tidytext %>% filter(!word %in% c(stopwords('french'),'a','très','d\'un','qu\'il','nc','avoir','afin'))
+      word_cors <- test_clean_df %>%
+        group_by(word) %>%
+        filter(n() >= 5) %>%
+        pairwise_cor(word, line, sort = TRUE)
+    })
 
     hues <- c(60:330)
     
@@ -50,7 +78,7 @@ server <- function(input, output) {
         })
 
         
-        #wordcloud
+        #wordcloud word plot
         output$wordPlot <- renderWordcloud2({
             freq <- var()
             validate(
@@ -97,23 +125,24 @@ server <- function(input, output) {
                 
                 test_feel_df <- feel()
                 test_feel_df = as.data.frame(test_feel_df)
-                test_feel_df[,11] = NA
+                test_feel_df[,12] = NA
                 for(i in 1:nrow(test_feel_df)){
-                    for(j in 5:10){
+                    for(j in 6:11){
                         if (test_feel_df[i,j]==1){
-                            test_feel_df[i,11] = colnames(test_feel_df)[j]
+                            test_feel_df[i,12] = colnames(test_feel_df)[j]
                         }
                     }
                 }
                 
-                colnames(test_feel_df)[11]="sentiment"
                 
+                colnames(test_feel_df)[12]="sentiment"
                 #Compte le nombre d'occurence et son sentiment (joy, anger...)
+                
                 occur_sentiment2 = test_feel_df %>%
                     count(word, sentiment, sort = TRUE)
                 
                 occur_sentiment2=occur_sentiment2 %>% filter(!is.na(sentiment))
-                
+
                 #graphique occurence par sentiment
                 occur_sentiment2 %>%
                     group_by(sentiment) %>%
@@ -150,31 +179,149 @@ server <- function(input, output) {
                 test_feel_df <- feel()
                 par(mar = rep(0, 4))
                 
-                test_feel_df <- feel()
                 test_feel_df = as.data.frame(test_feel_df)
-                test_feel_df[,11] = NA
+                test_feel_df[,12] = NA
                 for(i in 1:nrow(test_feel_df)){
-                    for(j in 5:10){
-                        if (test_feel_df[i,j]==1){
-                            test_feel_df[i,11] = colnames(test_feel_df)[j]
-                        }
+                  for(j in 6:11){
+                    if (test_feel_df[i,j]==1){
+                      test_feel_df[i,12] = colnames(test_feel_df)[j]
                     }
+                  }
                 }
                 
-                colnames(test_feel_df)[11]="sentiment"
+                
+                colnames(test_feel_df)[12]="sentiment"
                 
                 test_feel_df %>%
                     count(word, sentiment, sort = TRUE) %>%
+                    filter(!is.na(sentiment)) %>%
                     acast(word ~ sentiment, value.var = "n", fill = 0) %>%
                     comparison.cloud(colors = c("darkred", "darkgreen","purple","darkblue", "darkgray","magenta"),
                                      max.words = input$max2)
             }
         })
 
+        output$ngramPlot <- renderPlot({
+          
+          bigram_counts <- ngram()
+          bigram_graph <- bigram_counts %>%
+            filter(n > 1) %>%
+            graph_from_data_frame()
 
+          set.seed(2017)
+          a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+          
+          ggraph(bigram_graph, layout = "fr") +
+            geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                           arrow = a, end_cap = circle(.07, 'inches')) +
+            geom_node_point(color = "lightblue", size = 5) +
+            geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+            theme_void()
+        })
 
+        output$bigramWordCloud2 <- renderWordcloud2({
+          
+          bigram_counts <- ngram()
+          
+          
+          bigrams_united <- bigram_counts %>%
+            unite(bigram, word1, word2, sep = " ")
+          
+          validate(
+            need(input$max <= nrow(bigrams_united), "Selected size greater than number of elements in data")
+          )
+          new_df <- bigrams_united[1:input$max3,]
+          wordcloud2(new_df, size = .3, shuffle=T, rotateRatio = sample(c(1:100) / 100))
+          
+        })
         
+        output$bigramBarplot <- renderPlot({
+
+          bigram_counts <- ngram()
+          
+          
+          bigrams_united <- bigram_counts %>%
+            unite(bigram, word1, word2, sep = " ")
+          
+          new_df <- bigrams_united[1:input$numWords3,]
+
+          ggplot(new_df, aes(x=reorder(bigram, n), y=n, fill = as.factor(bigram))) +
+            geom_bar(stat = "identity", position = "dodge", col= "black") + xlab("Bigram") + ylab("Frequency") +
+            coord_flip() + scale_fill_hue(c = sample(hues, 1)) + guides(fill=FALSE) + ggtitle("Frequency of each Bigram") + 
+            theme(plot.title = element_text(color = "red", size = 12, face = "bold", hjust = 0.5))
+          
+        })
+        
+        #wordcloud generation
+        observeEvent(input$newCloud2, handlerExpr = {
+          output$bigramWordCloud2 <- renderWordcloud2({
+            
+            bigram_counts <- ngram()
+            
+            
+            bigrams_united <- bigram_counts %>%
+              unite(bigram, word1, word2, sep = " ")
+            
+            validate(
+              need(input$max <= nrow(bigrams_united), "Selected size greater than number of elements in data")
+            )
+            new_df <- bigrams_united[1:input$max3,]
+            wordcloud2(new_df, size = .3, shuffle=T, rotateRatio = sample(c(1:100) / 100))
+            
+          })
+        })
       
+        output$corrPlot <- renderPlot({
+          
+          word_cors <- corr()
+          
+          word_cors %>%
+            filter(correlation > input$phiNum) %>%
+            graph_from_data_frame() %>%
+            ggraph(layout = "fr") +
+            geom_edge_link(aes(edge_alpha = correlation), show.legend = FALSE) +
+            geom_node_point(color = "lightblue", size = 5) +
+            geom_node_text(aes(label = name), repel = TRUE) +
+            theme_void()
+
+          
+        })
         
+
+        
+        output$corrTable <- renderTable({
+          
+          word_cors <- corr()
+          
+          as.data.frame(word_cors %>%
+            filter(correlation > input$phiTable) %>%
+            filter(item1 == input$word))
+        })
+        
+        observeEvent(input$randomWord, handlerExpr = {
+            word_cors <- corr()
+              ls=unlist(word_cors[,1])
+              ls=unname(ls)
+              word=sample(ls,size=1,replace=F)
+              updateTextInput(session, "word", value=word)
+        })
+        
+        
+        output$corrPlotTable <- renderPlot({
+          
+          word_cors <- corr()
+          
+          word_cors %>%
+            filter(item1 == input$word) %>%
+            group_by(item1) %>%
+            top_n(input$corrNum) %>%
+            ungroup() %>%
+            mutate(item2 = reorder(item2, correlation)) %>%
+            ggplot(aes(item2, correlation, fill=as.factor(item2))) +
+            geom_bar(stat = "identity") +
+            facet_wrap(~ item1, scales = "free") +
+            coord_flip() + guides(fill=FALSE)
+          
+        })
 
 }
--- Projet_TM/ui.R
+++ Projet_TM/ui.R
@@ -9,6 +9,9 @@ library(wordcloud)
 library(wordcloud2)
 library(reshape2)
 library(plotly)
+library(igraph)
+library(ggraph)
+library(widyr)
 library(shiny)
 
 ui <- shinyUI(
@@ -39,8 +42,8 @@ ui <- shinyUI(
                              helpText("blablabla"),
                              hr(),
                              #varSelectInput("variable", "Variable:", text_df),
-                             selectInput("selectVar", label = h3("Variable to select"),
-                                         choices = list("Preoccupation" = 2, "Motif de consultation" = 3),
+                             selectInput("selectVar", label = h3("Variable to select :"),
+                                         choices = list("Motif de consultation" = 2, "Preoccupations" = 3),
                                          selected = 2),
                              sliderInput("numWords", "Words in bar graph:", 
                                          min=1, max= 35, value=10, step = 1),
@@ -54,8 +57,8 @@ ui <- shinyUI(
             conditionalPanel(condition="input.tabselected==2",
                              hr(),
                              helpText("blablabla"),
-                             selectInput("selectVar2", label = h3("Variable to select"),
-                                         choices = list("Preoccupation" = 2, "Motif de consultation" = 3),
+                             selectInput("selectVar2", label = h3("Variable to select :"),
+                                         choices = list("Motif de consultation" = 2, "Preoccupations" = 3),
                                          selected = 2),
                              radioButtons("radioSentiment", label = h3("Type of feelings"),
                                           choices = list("Positive/Negative" = 1, "Detailed Feelings" = 2), 
@@ -64,7 +67,37 @@ ui <- shinyUI(
                                         min=1, max= 35, value=10, step = 1),
                             sliderInput("max2",
                                         "Size of wordcloud:",
-                                        min = 5,  max = 100, value = 50, step = 5))),
+                                        min = 5,  max = 100, value = 50, step = 5)),
+            #side panel 3
+            conditionalPanel(condition="input.tabselected==3",
+                         hr(),
+                         helpText("blablabla"),
+                         selectInput("selectVar3", label = h3("Variable to select :"),
+                                     choices = list("Motif de consultation" = 2, "Preoccupations" = 3),
+                                     selected = 2),
+                         sliderInput("numWords3", "Words in barplot :", 
+                                     min=1, max= 35, value=6, step = 1),
+                         sliderInput("max3",
+                                     "Size of wordcloud :",
+                                     min = 5,  max = 100, value = 50, step = 5),
+                         actionButton("newCloud2", "Generate new wordcloud")),
+            
+            #side panel 4
+            conditionalPanel(condition="input.tabselected==4",
+                             hr(),
+                             helpText("blablabla"),
+                             selectInput("selectVar4", label = h3("Variable to select :"),
+                                         choices = list("Motif de consultation" = 2, "Preoccupations" = 3),
+                                         selected = 2),
+                             sliderInput("phiNum", "Minimum correlation :", 
+                                         min=0.05, max= 0.9, value=0.4, step = 0.05),
+                             textInput(inputId = "word",
+                                       label = "Find word associations among data:", value = "retard"),
+                             actionButton("randomWord", "Generate word from corpus"),
+                             sliderInput("phiTable", "Correlation Limit for table :", 
+                                         min=0.05, max= 0.9, value=0.2, step = 0.05),
+                             sliderInput("corrNum", "Words in bar graph:", 
+                                         min=1, max= 35, value=5, step = 1))),
 
         
         #conditional main panel
@@ -72,8 +105,16 @@ ui <- shinyUI(
             tabsetPanel(
                 tabPanel("Word Plots", plotOutput("freqPlot"), wordcloud2Output("wordPlot"), value = 1,  
                          conditionalPanel(condition="input.tabselected==1")),
-                tabPanel("Sentiments analysis", value = 2, conditionalPanel(condition="input.tabselected==2"), 
+                tabPanel("Sentiments Analysis", value = 2, conditionalPanel(condition="input.tabselected==2"), 
                          plotOutput("sentiment"), plotOutput("wordCloudSentiment")),
+                tabPanel("BiGram Visualization", value = 3,conditionalPanel(condition="input.tabselected==3"),
+                         plotOutput("ngramPlot"),fluidRow(
+                         column(6,plotOutput("bigramBarplot")),
+                         column(6,wordcloud2Output("bigramWordCloud2")))),
+                tabPanel("Word Correlation", value = 4, conditionalPanel(condition="input.tabselected==4"),
+                         plotOutput("corrPlot"),fluidRow(
+                             column(6,tableOutput("corrTable")),
+                             column(6,plotOutput("corrPlotTable")))),
                 id = "tabselected")
         )
         
